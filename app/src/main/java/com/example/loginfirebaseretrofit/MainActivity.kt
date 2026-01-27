c0ff83ca2be81d76f78dcd69c07ae54d8abbebab
GemaSN <gemassancheznavarro@gmail.com>
2026-01-27 12:56:41 +0100

    Diapo 128

--- app/src/main/java/com/example/loginfirebaseretrofit/MainActivity.kt
+++ app/src/main/java/com/example/loginfirebaseretrofit/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.loginfirebaseretrofit.home.HomeViewModel
 import com.example.loginfirebaseretrofit.login.ui.LoginScreen
 import com.example.loginfirebaseretrofit.login.ui.LoginViewModel
 import com.example.loginfirebaseretrofit.network.MarsApi
@@ -18,6 +19,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         var auth = FirebaseAuth.getInstance()
         val apiService = MarsApi.retrofitService
+
         enableEdgeToEdge()
         setContent {
             val navController = rememberNavController()
